71864cf52f0b3da47b6859b79d8398e00f6152ee
DaoToanh <daotoanhktxd@gmail.com>
2022-01-14 00:17:57 +0700

    Add Header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import { Navbar, NavbarBrand } from 'reactstrap'
+import { STAFFS } from './shared/StaffList'
 import './App.css'
 
 
 class App extends Component {
 
+  constructor(props) {
+    super(props)
 
+    this.state = {
+      staffs: STAFFS
+    }
+  }
 
   render() {
     return (
       <div className="App">
-         
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
+          </div>
+        </Navbar>
       </div>
     )
   }
