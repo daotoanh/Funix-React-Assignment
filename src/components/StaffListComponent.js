8dc258c23d5dedbc4050b3d561ca7642fdb357fe
DaoToanh <daotoanhktxd@gmail.com>
2022-01-14 00:29:46 +0700

    Add stafflist on page

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,23 +1,40 @@
 import React, { Component } from 'react'
-
+import { Card, CardText, CardTitle } from 'reactstrap'
+import dateFormat from 'dateformat'
 
 class StaffList extends Component {
 
     constructor(props) {
-          super(props)
-          
-    
+        super(props)
+
     }
 
-     render() {
+ 
 
-        
-         return (
-             <div className="container">
-             
-             </div>
-         )
-     }
+
+
+    render() {
+
+        const staffList = this.props.staffs.map((staff) => {
+            return (
+                <div className="col-lg-2 col-md-4  col-12">
+                    <Card key={staff.id}>
+                        <Card onClick={() => this.onStaffSelect(staff)}>
+                            <CardTitle>{staff.name}</CardTitle>
+                        </Card>
+                    </Card>
+                </div>
+            )
+        })
+
+        return (
+            <div className="container">
+                <div className="row">
+                    {staffList}
+                </div>
+            </div>
+        )
+    }
 }
 
 
