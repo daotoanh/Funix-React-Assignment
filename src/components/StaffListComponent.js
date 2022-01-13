7bd80c48310e9d448f645998e67c880b67422965
DaoToanh <daotoanhktxd@gmail.com>
2022-01-13 23:19:09 +0700

    Add StaffListComponent

---
+++ src/components/StaffListComponent.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+
+class StaffList extends Component {
+
+    constructor(props) {
+          super(props)
+          
+    
+    }
+
+     render() {
+
+        
+         return (
+             <div className="container">
+             
+             </div>
+         )
+     }
+}
+
+
+export default StaffList
