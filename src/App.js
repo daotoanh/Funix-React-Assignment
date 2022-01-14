80f990a62134c035d7f513a74299667015686f12
DaoToanh <daotoanhktxd@gmail.com>
2022-01-15 01:09:28 +0700

    Edit App.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,20 @@
 import React, { Component } from 'react'
-import Staffslist from './components/StaffListComponent'
-import { Navbar, NavbarBrand } from 'reactstrap'
-import { STAFFS } from './shared/StaffList'
+import Main from './components/StaffMainComponent'
 import './App.css'
 
 
 class App extends Component {
 
-  constructor(props) {
-    super(props)
 
-    this.state = {
-      staffs: STAFFS
-    }
-  }
 
   render() {
     return (
-      <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
-          </div>
-        </Navbar>
-        <Staffslist
-          staffs={this.state.staffs}
-        />
+      <div>
+        <Main />
       </div>
     )
   }
 }
-
 
 
 export default App;
