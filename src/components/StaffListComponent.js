f46c1c668e17c52a9e2d68f59fdca03d0bbb264d
DaoToanh <daotoanhktxd@gmail.com>
2022-01-18 21:45:55 +0700

    Done SearchStaff

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -22,6 +22,15 @@ function RenderStaffList({ staff, onClick }) {
 const StaffList = (props) => {
     const [staffs, setStaffs] = useState(props.staffs)
     const [searchStaff, setSearchStaff] = useState(" ")
+
+    const handleSreachChange = e => setSearchStaff(e.target.value)
+
+    const handleSubmit = e => {
+        let result = props.staffs.filter(staff => { return staff.name.toLowerCase().includes(searchStaff.toLowerCase()) })
+        setStaffs(result)
+        e.preventDefault()
+    }
+
     
 
     const staffList = staffs.map((staff) => {
@@ -40,11 +49,12 @@ const StaffList = (props) => {
                 <div className='col-3'>
                 </div>
                 <div className='col-8'>
-                    <Form>
+                    <Form onSubmit={e => handleSubmit(e)}>
                         <div className='row headersearch'>
                             <div className='col-6'>
                                 <Input type="text" id="searchname" name="searchname" placeholder="Tên nhân viên..."
-                                    />
+                                value={searchStaff}
+                                onChange={(e) => handleSreachChange(e)}    />
                             </div>
                             <div className='col-1'>
                                 <Button type="submit" value="submit" color="primary">Tìm</Button>
